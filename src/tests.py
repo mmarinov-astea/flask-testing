89ea36a27f9147d1e85dfc1ed969c25701840e83
mmarinov <mmarinov@astea.net>
2024-03-28 17:13:25 +0200

    Add simple test

--- src/tests.py
+++ src/tests.py
@@ -1,2 +1,16 @@
-if __name__ == "__main__":
-    print("HEY")
+import unittest
+from app import app
+
+class TestFlaskApp(unittest.TestCase):
+    
+    def setUp(self):
+        app.testing = True
+        self.app = app.test_client()
+
+    def test_hello(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode(), "Hello, Martin")
+
+if __name__ == '__main__':
+    unittest.main()
